0f3e0fa58d42a98e02af649759857a2cd45a2876
pellog1 <89632702+pellog1@users.noreply.github.com>
2024-09-12 12:23:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
               gasLimit: 2000000, // adjustable
               gasPrice,
               from: wallet.address,
-              value: parseUnits('0.0001', 'ether'), // adjustable
+              value: parseUnits('0.01', 'ether'), // adjustable
             };
 
             const result = await wallet.sendTransaction(transaction);
